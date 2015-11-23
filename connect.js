988e70eef8e0edad443dd782c2228cd529baf849
1205765 <bobocobo2112@gmail.com>
2015-11-23 14:34:04 +0000

    m10

--- connect.js
+++ connect.js
@@ -1,34 +1,13 @@
 /**
  * Created by 1205765 on 23/11/2015.
  */
-
 var mongodb = require('mongodb');
+//and our HTTP server
 var http = require('http');
+//setup our port
 var port = process.env.PORT || 1337;
 
 var url =  'mongodb://<bobo>:<letmein21122112>@ds054288.mongolab.com:54288/nodeappdb';
+
 var MongoClient = mongodb.MongoClient;
 
-http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Connecting \n');
-    // Use connect method to connect to the Server
-    MongoClient.connect(url, function (err, db) {
-        response.write('Connection Made \n');
-        if (err) {
-            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
-            //Error so close connection
-            db.close();
-        } else {
-            //HURRAY!! We are connected. :)
-            response.write('Connection established to' + url +"\n");
-
-            // do some work here with the database.
-
-            //Done Close connection
-            db.close();
-        }
-        response.end('Finished, Connection closed \n');
-    });
-
-}).listen(port);
--- server.js
+++ server.js
@@ -2,4 +2,26 @@
  * Created by 1205765 on 23/11/2015.
  */
 
-require('./connect');
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+
+            // do some work here with the database.
+
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
+
+}).listen(port);
