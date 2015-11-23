15c730b5b86296332ee94014c45cceb6a121c63d
1205765 <bobocobo2112@gmail.com>
2015-11-23 14:30:27 +0000

    m10

--- connect.js
+++ connect.js
@@ -1,18 +1,13 @@
 /**
  * Created by 1205765 on 23/11/2015.
  */
+
 var mongodb = require('mongodb');
-//and our HTTP server
 var http = require('http');
-//setup our port
 var port = process.env.PORT || 1337;
 
 var url =  'mongodb://<bobo>:<letmein21122112>@ds054288.mongolab.com:54288/nodeappdb';
-
 var MongoClient = mongodb.MongoClient;
-
-var http = require ('http')
-var port = process.evn.PORT || 1337;
 
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
