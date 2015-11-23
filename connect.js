7befed684c56566f5884fa4a6ad721ef25d836e7
1205765 <bobocobo2112@gmail.com>
2015-11-23 14:36:47 +0000

    m11

--- connect.js
+++ connect.js
@@ -11,3 +11,26 @@ var url =  'mongodb://<bobo>:<letmein21122112>@ds054288.mongolab.com:54288/nodea
 
 var MongoClient = mongodb.MongoClient;
 
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+
+            // do some work here with the database.
+
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
+
+}).listen(port);
