77a43b3486a6fb332b8a5d7b09a3849f5f0999dc
1205765 <bobocobo2112@gmail.com>
2015-11-23 14:07:57 +0000

    m9

--- server.js
+++ server.js
@@ -2,10 +2,23 @@
  * Created by 1205765 on 23/11/2015.
  */
 
-var http = require ('http')
-var port = process.evn.PORT || 1337;
+var http = require('http')
+var port = process.env.PORT || 1337;
 
-http.createServer(function(request,responce){
-    responce.writeHead(200,{'Content-Type' : 'text/plain'});
-    responce.end('hello m89\n');
-}).listen(port);
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<!DOCTYPE 'html'>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello World Page</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write("<h1>");
+    response.write("Hello World!");
+    response.write("</h1>");
+    response.write("</body>");
+    response.write("</html>");
+    response.end();
+});
+server.listen(port);
+console.log("Server is listening");
