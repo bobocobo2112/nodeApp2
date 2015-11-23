e6eccf07d367b795379042e5363d59ddfae58b1d
1205765 <bobocobo2112@gmail.com>
2015-11-23 14:01:43 +0000

    m8h

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var http = require ('http')
 var port = process.evn.PORT || 1337;
 
-http.createServer(function(request,responce)){
+http.createServer(function(request,responce){
     responce.writeHead(200,{'Content-Type' : 'text/plain'});
     responce.end('hello m8\n');
-}.listen(port);
+}).listen(port);
