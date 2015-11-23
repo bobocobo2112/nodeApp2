e5564c1411efe9f049a7b598e5ce9b668f5ea4cf
1205765 <bobocobo2112@gmail.com>
2015-11-23 14:00:25 +0000

    m8

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 /**
  * Created by 1205765 on 23/11/2015.
  */
+
+var http = require ('http')
+var port = process.evn.PORT || 1337;
+
+http.createServer(function(request,responce)){
+    responce.writeHead(200,{'Content-Type' : 'text/plain'});
+    responce.end('hello m8\n');
+}.listen(port);
